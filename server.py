277641a19d11b5b00639c9e107fb42e70c1df62b
Gabriel Gomes <107153200+GabrielCGM@users.noreply.github.com>
2022-08-11 23:25:16 -0300

    Delete server.py

--- server.py
+++
@@ -1,7 +0,0 @@
-def app(amb, start_response):
-    arq = open('cadastro.html', 'rb')
-    data = arq.read()
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/html')]
-    start_response(status, response_headers)
-    return [data]
